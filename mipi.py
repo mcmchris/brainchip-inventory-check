12da32b3defe7426a163868598e58b88ae7c35a7
Christopher Méndez <mcmchris@hotmail.com>
2024-06-14 17:18:39 -0400

    model v5

--- mipi.py
+++ mipi.py
@@ -192,8 +192,7 @@ def inferencing(model_file, queueOut):
         for event in power_events:
             active_power += event.power
     
-        power_consumption = (active_power/len(power_events)) - floor_power
-         
+        power_consumption = f'{(active_power/len(power_events)) - floor_power : 0.2f}' 
         #print(akida_model.statistics)
 
         result = fill_result_struct_f32_fomo(pred, int(EI_CLASSIFIER_INPUT_WIDTH/8), int(EI_CLASSIFIER_INPUT_HEIGHT/8))
