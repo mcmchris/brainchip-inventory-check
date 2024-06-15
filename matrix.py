22c0a385e2c9d1bc0e0ebde1ea41ebb778c96d0a
Christopher Méndez <mcmchris@hotmail.com>
2024-06-15 19:55:24 -0400

    matrix update

--- matrix.py
+++ matrix.py
@@ -6,16 +6,16 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 print("I2C devices found: ", [hex(i) for i in i2c.scan()])
 
-Seeed_Matrix = 0x65
+matrix = 0x65
 
-if not Seeed_Matrix in i2c.scan():
+if not matrix in i2c.scan():
     print("Could not find Seeed Matrix")
     sys.exit()
 
 def getDeviceVID():
-    i2c.writeto(Seeed_Matrix, bytes([0x00]))
+    i2c.writeto(matrix, bytes([0x00]))
     result = bytearray(1)
-    i2c.readfrom_into(Seeed_Matrix, result)
+    i2c.readfrom_into(matrix, result)
     print("Seeed Matrix ID: ", hex.from_bytes(result,"big"))
 
 if __name__ == "__main__":
