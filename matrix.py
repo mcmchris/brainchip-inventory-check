5a0e49f4d9da4449968a6e173bbe26788870afcf
Christopher Méndez <mcmchris@hotmail.com>
2024-06-15 20:30:45 -0400

    matrix update

--- matrix.py
+++ matrix.py
@@ -22,6 +22,23 @@ def getDeviceVID():
     print("Seeed Matrix ID: ", vid)
     return vid
 
+def stopDisplay():
+    i2c.writeto(matrix, bytes([0x06]))
+
+
+def displayColorBlock(rgb, duration_time, forever_flag):
+    data= {0,}
+    data[0] = 0x0d #I2C_CMD_DISP_COLOR_BLOCK
+    data[1] = (rgb >> 16) & 0xff
+    data[2] = (rgb >> 8) & 0xff
+    data[3] = rgb & 0xff
+    data[4] = (duration_time & 0xff)
+    data[5] = ((duration_time >> 8) & 0xff)
+    data[6] = forever_flag
+    print(hex(data))
+
+
+
 if __name__ == "__main__":
     VID = getDeviceVID()
     if VID != '0x8628':
@@ -29,3 +46,5 @@ if __name__ == "__main__":
         sys.exit()
     
     print("Matrix init success")
+
+    stopDisplay()
