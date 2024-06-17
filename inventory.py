ad884dffd2952e2440faee8c53a79ad790ad48f0
Christopher Méndez <mcmchris@hotmail.com>
2024-06-17 16:45:03 -0400

    mipi update

--- inventory.py
+++ inventory.py
@@ -6,6 +6,12 @@ import signal
 import threading
 import sys
 import numpy as np
+
+# For I2C communication
+import board
+import busio
+import time
+
 from queue import Queue
 from scipy.special import softmax
 from flask import Flask, render_template, Response
@@ -18,7 +24,9 @@ normalSize = (640 , 480)
 lowresSize = (224, 224)
 
 app = Flask(__name__, static_folder='templates/assets')
-    
+
+i2c = busio.I2C(board.SCL, board.SDA)
+        
 EI_CLASSIFIER_INPUT_WIDTH  = 224
 EI_CLASSIFIER_INPUT_HEIGHT = 224
 EI_CLASSIFIER_LABEL_COUNT = 1
@@ -202,12 +210,24 @@ def inferencing(model_file, queueOut):
         result = fill_result_struct_f32_fomo(pred, int(EI_CLASSIFIER_INPUT_WIDTH/8), int(EI_CLASSIFIER_INPUT_HEIGHT/8))
         
         #print(result)
+        picTwo = [255]*64
 
         for bb in result['bounding_boxes']:
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] + int(bb['height']/2)) * scale_out_y)), 8, (57, 255, 20), 2)
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] +  int(bb['height']/2)) * scale_out_y)), 4, (255, 165, 0), 2)
-            print(bb['x'], bb['y'])
+            
+            x = bb['x']
+            y = bb['y']
+
+            print(x,y)
+
+            x = int(x/224)
+            y = int(y/224)
+                
+            picTwo[xytoIndex(x,y)] = 55
         
+        displayFrames(picTwo, 500, True, 1)
+
         piece_count = result['bounding_boxes_count']
         #piece_count = len(result['bounding_boxes'])
         #print(piece_count)
@@ -250,6 +270,66 @@ def get_fps():
         yield "data:" + str(akida_fps) + "\n\n"
         time.sleep(1)
 
+def getDeviceVID():
+    i2c.writeto(matrix, bytes([0x00]))
+    result = bytearray(1)
+    i2c.readfrom_into(matrix, result)
+    vid = hex(int.from_bytes(result))
+    print("Seeed Matrix ID: ", vid)
+    return vid
+
+def stopDisplay():
+    i2c.writeto(matrix, bytes([0x06]))
+
+
+def displayColorBlock(rgb, duration_time, forever_flag):
+    data=[0]*7
+    data[0] = 0x0d #I2C_CMD_DISP_COLOR_BLOCK
+    data[1] = (rgb >> 16) & 0xff
+    data[2] = (rgb >> 8) & 0xff
+    data[3] = rgb & 0xff
+    data[4] = (duration_time & 0xff)
+    data[5] = ((duration_time >> 8) & 0xff)
+    data[6] = forever_flag
+    result = [hex(i) for i in data]
+    i2c.writeto(matrix, bytes(data))
+    #print(result)
+
+def displayFrames(buffer, duration_time, forever_flag, frames_number):
+    data=[0]*72
+    if frames_number > 5:
+        frames_number = 5
+    elif frames_number == 0:
+        return
+    
+    data[0] = 0x05 #I2C_CMD_DISP_CUSTOM
+    data[1] = 0x0
+    data[2] = 0x0
+    data[3] = 0x0
+    data[4] = frames_number
+
+    for i in range(frames_number):
+        data[5] = i
+        for j in range(64):
+            data[8+j] = buffer[j+i*64]
+        if i == 0:
+            data[1] = (duration_time & 0xff)
+            data[2] = ((duration_time >> 8) & 0xff)
+            data[3] = forever_flag
+
+        result = [hex(i) for i in data]
+        #print(result)
+        i2c.writeto(matrix, bytes(data))
+
+
+def xytoIndex(x, y):
+    if x > 8 or x < 0 or y > 8 or y < 0:
+        print("Axis out of range")
+        return
+    row = (8-y)*8 - 8
+    index = row + x
+    return index
+
 @app.route('/video_feed')
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
@@ -276,7 +356,21 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+
     model_file = './model/akida_model.fbz'
+
+    scan_result = i2c.scan()
+
+    print("I2C devices found: ", [hex(i) for i in scan_result])
+
+    matrix = 0x65
+
+    if not matrix in scan_result:
+        print("Could not find Seeed Matrix")
+        sys.exit()
+
+    stopDisplay()
+    time.sleep(1)
 
     queueOut = Queue(maxsize = 24)
 
