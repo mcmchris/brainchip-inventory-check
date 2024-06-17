0bdfc31b38edfaf4d35dcb732ebb7e3b6b628018
Christopher Méndez <mcmchris@hotmail.com>
2024-06-17 16:26:15 -0400

    mipi update

--- mipi.py
+++ mipi.py
@@ -201,11 +201,12 @@ def inferencing(model_file, queueOut):
 
         result = fill_result_struct_f32_fomo(pred, int(EI_CLASSIFIER_INPUT_WIDTH/8), int(EI_CLASSIFIER_INPUT_HEIGHT/8))
         
-        print(result)
+        #print(result)
 
         for bb in result['bounding_boxes']:
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] + int(bb['height']/2)) * scale_out_y)), 8, (57, 255, 20), 2)
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] +  int(bb['height']/2)) * scale_out_y)), 4, (255, 165, 0), 2)
+            print(bb['x'], bb['y'])
         
         piece_count = result['bounding_boxes_count']
         #piece_count = len(result['bounding_boxes'])
