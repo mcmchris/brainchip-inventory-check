b1581f1b6e5203f9f998e2d2f5ddb17bf4052c9e
Christopher Méndez <mcmchris@hotmail.com>
2024-06-20 14:44:01 -0400

    update

--- inventory.py
+++ inventory.py
@@ -224,10 +224,10 @@ def inferencing(model_file, queueOut):
                 med = med+1
             else:
             #print(x,y)
-                x = int(x/10)
-                y = int(y/10)
+                xi = int(x/10)
+                yi = int(y/10)
+                picTwo[xytoIndex(xi,yi)] = 55
                 med = 0
-                picTwo[xytoIndex(x,y)] = 55
         
         displayFrames(picTwo, 500, True, 1)
 
