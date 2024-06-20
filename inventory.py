c7945a12a9004244ea2b1259b6683659fc41b437
Christopher Méndez <mcmchris@hotmail.com>
2024-06-20 15:31:22 -0400

    update

--- inventory.py
+++ inventory.py
@@ -37,7 +37,7 @@ inference_speed = 0
 power_consumption = 0
 piece_count = 0
 akida_fps = 0
-
+picTwo = [255]*64
 
 def ei_cube_check_overlap(c, x, y, width, height, confidence):
     is_overlapping = not ((c['x'] + c['width'] < x) or (c['y'] + c['height'] < y) or (c['x'] > x + width) or (c['y'] > y + height))
@@ -176,6 +176,7 @@ def inferencing(model_file, queueOut):
 
     picam2.start()
 
+    printit()
         
     resize_dim = (EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT)
 
@@ -213,7 +214,7 @@ def inferencing(model_file, queueOut):
         result = fill_result_struct_f32_fomo(pred, int(EI_CLASSIFIER_INPUT_WIDTH/8), int(EI_CLASSIFIER_INPUT_HEIGHT/8))
         
         #print(result)
-        picTwo = [255]*64
+        
   
         for bb in result['bounding_boxes']:
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] + int(bb['height']/2)) * scale_out_y)), 8, (57, 255, 20), 2)
@@ -228,7 +229,7 @@ def inferencing(model_file, queueOut):
             picTwo[xytoIndex(x,y)] = 55
              
         
-        displayFrames(picTwo, 500, True, 1)
+        
 
         piece_count = result['bounding_boxes_count']
         #piece_count = len(result['bounding_boxes'])
@@ -238,7 +239,9 @@ def inferencing(model_file, queueOut):
         if not queueOut.full():
             queueOut.put(img)
 
-        
+def printit():
+  threading.Timer(5.0, printit).start()
+  displayFrames(picTwo, 500, True, 1)       
         
 def gen_frames():
     #resize_stream = (640, 480)
