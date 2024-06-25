6ca44aa1d68cc3faf44e60706610fff6b886669c
Christopher Méndez <mcmchris@hotmail.com>
2024-06-25 17:24:10 -0400

    crop test

--- stock.py
+++ stock.py
@@ -167,7 +167,8 @@ def inferencing(model_file, queueOut):
     picam2 = Picamera2()
     #picam2.start_preview(Preview.DRM, x=0, y=0, width=1920, height=1080)
     picam2.start_preview(Preview.NULL)
-    config = picam2.create_preview_configuration(main={"size": normalSize}, lores={"size": lowresSize, "format": "RGB888"})
+    config = picam2.create_preview_configuration(main={"size": normalSize}, 
+                                                 lores={"size": lowresSize, "format": "RGB888"})
 
     picam2.configure(config)
 
