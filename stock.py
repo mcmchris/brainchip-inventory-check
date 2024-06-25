f860b87e7cbcdcf3893aabef11d7dcb7a13d21d2
Christopher Méndez <mcmchris@hotmail.com>
2024-06-25 15:36:25 -0400

    camera test

--- stock.py
+++ stock.py
@@ -182,7 +182,7 @@ def inferencing(model_file, queueOut):
     resize_dim = (EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT)
 
     while True:
-        frame = picam2.capture_array("main")
+        frame = picam2.capture_array("lores")
         #img = picam2.capture_array("lores")
         #cropped_img = frame[0:720, 280:280+720]
         #resized_img = cv2.resize(frame, resize_dim, interpolation = cv2.INTER_AREA)
