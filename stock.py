5812b809cd48bbebcd75765f754683bc6290a612
Christopher Méndez <mcmchris@hotmail.com>
2024-07-02 15:06:02 -0400

    input size get

--- stock.py
+++ stock.py
@@ -180,6 +180,8 @@ def inferencing(model_file, queueOut):
 
         input_data = np.expand_dims(img, axis=0)
         
+        print("Input_data size in bytes: ", sys.getsizeof(input_data))
+
         start_time = time.perf_counter()
         logits = akida_model.predict(input_data)
         end_time = time.perf_counter()
